1f144227405a87167a725bba50259163cd790c06
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-06-05 12:10:18 +0300

    feat: add link and basic copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,92 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link, { LinkProps } from "next/link";
+import { GitHub, Twitter, Linkedin, Icon } from "react-feather";
+
+const GITHUB_LINK = "https://github.com/meronogbai";
+const TWITTER_LINK = "https://twitter.com/meronogbai";
+const LINKEDIN_LINK = "https://www.linkedin.com/in/meronogbai";
+const EMAIL = "mailto:okbaymeron@gmail.com";
+const RESUME =
+  "https://docs.google.com/document/d/1wkbHhGV8NKb8BE4BDHTmr0h7OQZbgs-r6v5theUWvJ4/export?format=pdf";
+
+const SocialLink = ({
+  href,
+  children,
+}: {
+  href: LinkProps["href"];
+  children: React.ReactNode;
+}) => {
+  return (
+    <Link href={href}>
+      <a className="hover:text-blue-600 transition-colors" target="_blank">
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+const SocialLinks = () => (
+  <div className="flex gap-2">
+    <SocialLink href={GITHUB_LINK}>
+      <GitHub />
+    </SocialLink>
+
+    <SocialLink href={TWITTER_LINK}>
+      <Twitter />
+    </SocialLink>
+
+    <SocialLink href={LINKEDIN_LINK}>
+      <Linkedin />
+    </SocialLink>
+  </div>
+);
+
+const Contact = () => (
+  <Link href={EMAIL}>
+    <a className="transition-colors border border-blue-600 p-2 bg-blue-600 hover:bg-black rounded">
+      Contact Me
+    </a>
+  </Link>
+);
+
+const GetResume = () => (
+  <Link href={RESUME}>
+    <a className="transition-colors border border-blue-600 p-2 hover:bg-blue-600 rounded">
+      Get Resume
+    </a>
+  </Link>
+);
+
+const Ctas = () => (
+  <div className="flex gap-6">
+    <Contact />
+    <GetResume />
+  </div>
+);
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col py-2 bg-black text-gray-100">
       <Head>
-        <title>Create Next App</title>
+        <title>Meron Ogbai</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex w-full flex-1 flex-col justify-center px-20 ">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-6xl font-bold">
+            Hi , I'm <span className="text-blue-600">Meron</span>
+          </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <p className="text-2xl">
+            I'm a software engineer who loves building things for the web.
+          </p>
 
+          <SocialLinks />
+          <Ctas />
+        </div>
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
@@ -68,19 +130,11 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex justify-end pt-2 pr-4 w-full border-t border-gray-700">
+        <SocialLinks />
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
