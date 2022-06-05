d59c38f7be0004c584417f877b8734e05dede6ac
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-06-05 12:25:41 +0300

    feat: cta icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link, { LinkProps } from "next/link";
-import { GitHub, Twitter, Linkedin, Icon } from "react-feather";
+import { GitHub, Twitter, Linkedin, Mail, FileText } from "react-feather";
 
 const GITHUB_LINK = "https://github.com/meronogbai";
 const TWITTER_LINK = "https://twitter.com/meronogbai";
@@ -45,16 +45,16 @@ const SocialLinks = () => (
 
 const Contact = () => (
   <Link href={EMAIL}>
-    <a className="transition-colors border border-blue-600 p-2 bg-blue-600 hover:bg-black rounded">
-      Contact Me
+    <a className="transition-colors border border-blue-600 hover:border-white p-2 bg-blue-600 hover:bg-black rounded flex gap-1">
+      <Mail /> <span>Contact Me</span>
     </a>
   </Link>
 );
 
 const GetResume = () => (
   <Link href={RESUME}>
-    <a className="transition-colors border border-blue-600 p-2 hover:bg-blue-600 rounded">
-      Get Resume
+    <a className="transition-colors border p-2 hover:bg-blue-600 hover:border-blue-600 rounded flex gap-1">
+      <FileText /> <span>Get Resume</span>
     </a>
   </Link>
 );
@@ -86,47 +86,6 @@ const Home: NextPage = () => {
 
           <SocialLinks />
           <Ctas />
-        </div>
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
