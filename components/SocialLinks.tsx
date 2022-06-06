2b64bfd769ff0009d087e00da2ef067e9d676ca6
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-06-06 12:06:54 +0300

    fix: a11y

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -8,13 +8,19 @@ const LINKEDIN_LINK = "https://www.linkedin.com/in/meronogbai";
 const SocialLink = ({
   href,
   children,
+  label,
 }: {
   href: LinkProps["href"];
   children: React.ReactNode;
+  label: string;
 }) => {
   return (
     <Link href={href}>
-      <a className="hover:text-blue-600 transition-colors" target="_blank">
+      <a
+        className="hover:text-blue-600 transition-colors"
+        target="_blank"
+        aria-label={label}
+      >
         {children}
       </a>
     </Link>
@@ -23,15 +29,15 @@ const SocialLink = ({
 
 export const SocialLinks = () => (
   <div className="flex gap-2">
-    <SocialLink href={GITHUB_LINK}>
+    <SocialLink href={GITHUB_LINK} label="GitHub">
       <GitHub />
     </SocialLink>
 
-    <SocialLink href={TWITTER_LINK}>
+    <SocialLink href={TWITTER_LINK} label="Twitter">
       <Twitter />
     </SocialLink>
 
-    <SocialLink href={LINKEDIN_LINK}>
+    <SocialLink href={LINKEDIN_LINK} label="Linkedin">
       <Linkedin />
     </SocialLink>
   </div>
