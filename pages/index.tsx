b2d62dc4e0a0f6708602b3aff0a4545846265523
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-12-06 03:28:48 +0300

    feat: improve layout for large screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,25 +8,23 @@ import {
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col py-2 text-gray-100">
-      <main className="flex w-full flex-1 flex-col justify-center px-3 sm:px-20 space-y-12">
-        <section className="flex flex-col gap-6">
-          <h1 className="text-6xl font-bold">
-            Hi, I'm <span className="text-blue-600">Meron</span>
-          </h1>
+    <main className="p-3 sm:p-20 text-gray-100 max-w-5xl m-auto flex flex-col gap-12 justify-center min-h-screen">
+      <section className="flex flex-col gap-6">
+        <h1 className="text-6xl font-bold">
+          Hi, I'm <span className="text-blue-600">Meron</span>
+        </h1>
 
-          <p className="text-2xl">
-            I'm a software engineer who loves building things for the web.
-          </p>
+        <p className="text-2xl">
+          I'm a software engineer who loves building things for the web.
+        </p>
 
-          <SocialLinks />
-        </section>
+        <SocialLinks />
+      </section>
 
-        <CtaSection />
-        <ToolboxSection />
-        {/* <ProjectsSection /> */}
-      </main>
-    </div>
+      <CtaSection />
+      <ToolboxSection />
+      {/* <ProjectsSection /> */}
+    </main>
   );
 };
 
